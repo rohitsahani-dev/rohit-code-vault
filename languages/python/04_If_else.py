568e5579f50dc3177dfe90615abbaf90f70bd591
Rohit <rohit.sahani.dev@gmail.com>
2026-04-15 18:46:44 +0545

    How to use If else in python

---
+++ languages/python/04_If_else.py
@@ -0,0 +1,8 @@
+# If-else condition
+
+age = 18 
+#--Indentation is important in python, it defines the block of code
+if age >= 18:
+    print("Adult")  # prints "Adult" if age is 18 or more
+else:
+    print("Minor")
