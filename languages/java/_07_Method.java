fc223a3feda31cbc17a0ee8622cc1930327500cb
Rohit <rohit.sahani.dev@gmail.com>
2026-04-16 13:36:29 +0545

    How to use methon in java

---
+++ languages/java/_07_Method.java
@@ -0,0 +1,13 @@
+public class _07_Method {
+    // Methods (functions)
+    // Methods in Java are blocks of code that perform a specific task. 
+    // They are defined within a class and can be called to execute the code they contain.
+    static void greet(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    public static void main(String[] args) {
+        greet("Rohit");   // Calling the greet method with the argument "Rohit"
+    }
+}
+
