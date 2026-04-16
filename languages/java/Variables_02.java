1c45f8e7992eac6bfc53229ce1d4e67089c4725d
Rohit <rohit.sahani.dev@gmail.com>
2026-04-16 13:10:33 +0545

    How to use variable in java

---
+++ languages/java/Variables_02.java
@@ -0,0 +1,11 @@
+// Variables in Java
+
+public class Variables_02 {
+    public static void main(String[] args) {
+        String name = "Rohit";
+        int age = 20;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
