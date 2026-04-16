59edbecd3e90ba238979529f9d0080b02bd805ce
Rohit <rohit.sahani.dev@gmail.com>
2026-04-16 13:13:13 +0545

    How to use dataType in java

---
+++ languages/java/DataTypes_03.java
@@ -0,0 +1,15 @@
+// Basic data types
+
+public class DataTypes_03 {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 3.14;
+        char c = 'A';
+        boolean isTrue = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(isTrue);
+    }
+}
